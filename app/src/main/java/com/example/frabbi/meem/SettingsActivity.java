4d86f57b38d4614f098db4b15ce6fe938cc7902f
imm <malihanawshin@gmail.com>
2017-04-07 12:07:22 +0600

    settings buttons work , circular image option added with gallery access (from imm pc)

--- app/src/main/java/com/example/frabbi/meem/SettingsActivity.java
+++ app/src/main/java/com/example/frabbi/meem/SettingsActivity.java
@@ -1,17 +1,69 @@
 package com.example.frabbi.meem;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.view.inputmethod.InputMethodManager;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
 
 public class SettingsActivity extends BottomBarActivity {
 
     private EditText geteditname;
     private EditText getphoto;
     private RadioGroup getmode;
-    private Button btn_save;
+    private Button logoutBtn;
+    private Button saveChangeBtn;
+    private Button photoBtn;
+    private CircleImageView myimageview;
+
+    private static final int BROWSE_GALLERY_REQUEST_CODE = 100;
+
+    private View.OnClickListener clickListener = new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+
+                /*case R.id.auto:
+                    if(((RadioButton)v).isChecked())
+                        //save mode to auto
+                        break;
+
+                case R.id.manual:
+                    if(((RadioButton)v).isChecked())
+                        //save mode to manual
+                        break;
+*/
+                case R.id.browsePhoto:
+                    openImageGallery();
+                    break;
+
+                case R.id.saveChange:
+                    saveNewChange();
+                    break;
+
+                case R.id.logout:
+                    logoutFromAccount();
+                    break;
+            }
+        }
+    };
 
 
     @Override
@@ -24,9 +76,82 @@ public class SettingsActivity extends BottomBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getLayoutInflater().inflate(R.layout.activity_settings, frameContent);
+
         if(getSupportActionBar()!=null) getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         settings.setClickable(false);
+
+        logoutBtn = (Button) findViewById(R.id.logout);
+        logoutBtn.setOnClickListener(clickListener);
+
+        saveChangeBtn = (Button) findViewById(R.id.saveChange);
+        saveChangeBtn.setOnClickListener(clickListener);
+
+        photoBtn = (Button) findViewById(R.id.browsePhoto);
+        photoBtn.setOnClickListener(clickListener);
+
+        myimageview = (CircleImageView) findViewById(R.id.profilePhoto);
+        myimageview.setOnClickListener(clickListener);
+
+        /*getmode = (RadioGroup) findViewById(R.id.mode);
+        getmode.setOnClickListener((View.OnClickListener) this);
+
+        geteditname = (EditText) findViewById(R.id.editname);
+        geteditname.setOnClickListener((View.OnClickListener) this);
+
+        getphoto = (EditText)  findViewById(R.id.photo);
+        getphoto.setOnClickListener((View.OnClickListener)this);*/
+
     }
+
+    protected void saveNewChange(){
+        Toast.makeText(this, "Changes Saved !", Toast.LENGTH_SHORT).show();
+        return;
+    }
+
+    protected void logoutFromAccount(){
+        Toast.makeText(this, "Logged Out !", Toast.LENGTH_SHORT).show();
+        startActivity(new Intent(SettingsActivity.this,ActivityLogin.class));
+    }
+
+
+    private void openImageGallery() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_PICK);
+        if (getPackageManager().resolveActivity(intent, 0) != null) {
+            startActivityForResult(Intent.createChooser(intent, "Select Picture"), BROWSE_GALLERY_REQUEST_CODE);
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == BROWSE_GALLERY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Uri selectedImageUri = data.getData();
+                if (null != selectedImageUri) {
+                    String path = getRealPathFromURI(this, selectedImageUri);
+                    Toast.makeText(this,path,Toast.LENGTH_SHORT).show();
+                    myimageview.setImageBitmap(BitmapFactory.decodeFile(path));
+
+                }
+            }
+        }
+    }
+
+    public String getRealPathFromURI(Context context, Uri contentUri) {
+        Cursor cursor = null;
+        try {
+            String[] proj = { MediaStore.Images.Media.DATA };
+            cursor = context.getContentResolver().query(contentUri,  proj, null, null, null);
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+    }
+
 
 }
 
---
+++ app/src/test/java/com/example/frabbi/meem/SettingsActivityTest.java
@@ -0,0 +1,10 @@
+package com.example.frabbi.meem;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by imm on 4/7/2017.
+ */
+public class SettingsActivityTest {
+
+}
