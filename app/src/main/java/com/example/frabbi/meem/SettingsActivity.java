4f9afe743e49369a650bbba2145bf0e215b75cf4
imm <malihanawshin@gmail.com>
2017-04-19 01:24:34 +0600

    settings changed, bottombar icon changed, logo,photo added

--- app/src/main/java/com/example/frabbi/meem/SettingsActivity.java
+++ app/src/main/java/com/example/frabbi/meem/SettingsActivity.java
@@ -1,25 +1,23 @@
 package com.example.frabbi.meem;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.RequiresApi;
+import android.support.v7.widget.SwitchCompat;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.Toast;
-
-import android.content.Context;
-import android.content.Intent;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.provider.MediaStore;
-import android.view.inputmethod.InputMethodManager;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -27,31 +25,20 @@ import de.hdodenhof.circleimageview.CircleImageView;
 public class SettingsActivity extends BottomBarActivity {
 
     private EditText geteditname;
-    private EditText getphoto;
-    private RadioGroup getmode;
     private Button logoutBtn;
     private Button saveChangeBtn;
-    private Button photoBtn;
     private CircleImageView myimageview;
+    private SwitchCompat mode;
+    private Spinner time;
 
     private static final int BROWSE_GALLERY_REQUEST_CODE = 100;
 
-    private View.OnClickListener clickListener = new View.OnClickListener(){
+    private View.OnClickListener clickListener = new View.OnClickListener() {
+        @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
         @Override
         public void onClick(View v) {
-            switch (v.getId()){
-
-                /*case R.id.auto:
-                    if(((RadioButton)v).isChecked())
-                        //save mode to auto
-                        break;
-
-                case R.id.manual:
-                    if(((RadioButton)v).isChecked())
-                        //save mode to manual
-                        break;
-*/
-                case R.id.browsePhoto:
+            switch (v.getId()) {
+                case R.id.profilePhoto:
                     openImageGallery();
                     break;
 
@@ -69,7 +56,7 @@ public class SettingsActivity extends BottomBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId()==android.R.id.home) onBackPressed();
+        if (item.getItemId() == android.R.id.home) onBackPressed();
         return super.onOptionsItemSelected(item);
     }
 
@@ -78,7 +65,7 @@ public class SettingsActivity extends BottomBarActivity {
         super.onCreate(savedInstanceState);
         getLayoutInflater().inflate(R.layout.activity_settings, frameContent);
 
-        if(getSupportActionBar()!=null) getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        if (getSupportActionBar() != null) getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         settings.setClickable(false);
 
         logoutBtn = (Button) findViewById(R.id.logout);
@@ -87,39 +74,49 @@ public class SettingsActivity extends BottomBarActivity {
         saveChangeBtn = (Button) findViewById(R.id.saveChange);
         saveChangeBtn.setOnClickListener(clickListener);
 
-        photoBtn = (Button) findViewById(R.id.browsePhoto);
-        photoBtn.setOnClickListener(clickListener);
-
         myimageview = (CircleImageView) findViewById(R.id.profilePhoto);
         myimageview.setOnClickListener(clickListener);
-
-        /*getmode = (RadioGroup) findViewById(R.id.mode);
-        getmode.setOnClickListener((View.OnClickListener) this); */
 
         geteditname = (EditText) findViewById(R.id.editname);
         geteditname.setOnClickListener(clickListener);
 
+        time = (Spinner) findViewById(R.id.time);
 
-
+        mode = (SwitchCompat) findViewById(R.id.auto);
+        mode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                showSpinner(isChecked);
+            }
+        });
     }
 
-    protected void saveNewChange(){
+    protected void saveNewChange() {
 
         SharedPreferences sp = getSharedPreferences("ProfileName", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sp.edit();
-        editor.putString("ProfileName",geteditname.getText().toString());
+        editor.putString("ProfileName", geteditname.getText().toString());
         editor.commit();
 
         Toast.makeText(this, "Changes Saved !", Toast.LENGTH_SHORT).show();
         return;
     }
 
-    protected void logoutFromAccount(){
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    protected void logoutFromAccount() {
         ISystem.resetAccount(this);
         Toast.makeText(this, "Logged Out !", Toast.LENGTH_SHORT).show();
-        startActivity(new Intent(SettingsActivity.this,ActivityLogin.class));
+        finishAffinity();
+        startActivity(new Intent(SettingsActivity.this, ActivityLogin.class));
     }
 
+    protected void showSpinner(boolean toShow) {
+        if (toShow) {
+            time.setVisibility(View.VISIBLE);
+        } else {
+            time.setVisibility(View.GONE);
+        }
+    }
 
     private void openImageGallery() {
         Intent intent = new Intent();
@@ -139,10 +136,10 @@ public class SettingsActivity extends BottomBarActivity {
 
                     SharedPreferences sp = getSharedPreferences("profilePhoto", Context.MODE_PRIVATE);
                     SharedPreferences.Editor editor = sp.edit();
-                    editor.putString("image_path",path);
+                    editor.putString("image_path", path);
                     editor.commit();
 
-                    Toast.makeText(this,path,Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, path, Toast.LENGTH_SHORT).show();
                     myimageview.setImageBitmap(BitmapFactory.decodeFile(path));
 
                 }
@@ -153,8 +150,8 @@ public class SettingsActivity extends BottomBarActivity {
     public String getRealPathFromURI(Context context, Uri contentUri) {
         Cursor cursor = null;
         try {
-            String[] proj = { MediaStore.Images.Media.DATA };
-            cursor = context.getContentResolver().query(contentUri,  proj, null, null, null);
+            String[] proj = {MediaStore.Images.Media.DATA};
+            cursor = context.getContentResolver().query(contentUri, proj, null, null, null);
             int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
             cursor.moveToFirst();
             return cursor.getString(column_index);
@@ -164,7 +161,4 @@ public class SettingsActivity extends BottomBarActivity {
             }
         }
     }
-
-
 }
-
