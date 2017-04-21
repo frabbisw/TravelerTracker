b6ea460c55284bda16ea9d35ca672cc9d3246c80
Fazle Rabbi <frduiit@gmail.com>
2017-04-21 22:17:41 +0600

    /fr-21-2/

--- app/src/main/java/com/example/frabbi/meem/MapActivity.java
+++ app/src/main/java/com/example/frabbi/meem/MapActivity.java
@@ -3,15 +3,26 @@ package com.example.frabbi.meem;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class MapActivity extends BottomBarActivity implements OnMapReadyCallback {
 
     Account account=null;
+    GoogleMap mGoogleMap;
+    SupportMapFragment mapFrag;
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if(item.getItemId()==android.R.id.home) onBackPressed();
@@ -26,11 +37,47 @@ public class MapActivity extends BottomBarActivity implements OnMapReadyCallback
         if(getSupportActionBar()!=null) getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         account=ISystem.loadAccountFromCache(this);
+        mapFrag = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.viewMap);
+        mapFrag.getMapAsync(this);
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap)
+    public void onMapReady(final GoogleMap googleMap)
     {
+        Log.e("map","ready");
+        mGoogleMap=googleMap;
+        final Marker marker = mGoogleMap.addMarker(new MarkerOptions().position(new LatLng(90, 23)).title("My position"));
+        final Random random = new Random();
 
+        mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        new Thread()
+        {
+            public void run()
+            {
+                for(int i=0; i<10; i++)
+                {
+                    final double x = 23+random.nextDouble();
+                    final double y = 90+random.nextDouble();
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            LatLng lt = new LatLng(x,y);
+                            marker.setPosition(lt);
+                            mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(lt));
+                            mGoogleMap.animateCamera(CameraUpdateFactory.zoomTo(6));
+                        }
+                    });
+
+                    Log.e("M",Integer.toString(i));
+                    try {
+                        Thread.sleep(3000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }.start();
     }
 }
